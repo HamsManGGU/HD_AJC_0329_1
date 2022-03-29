6f418f14aac3ec92fab15b27a00a8a5a19526726
HAMJJUMAN <tupd1121@gmail.com>
2022-03-29 21:08:28 +0900

    혼자 해결해보기 1번 해결

---
+++ 0329_1/0329_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0329_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 3; i <= 9; i = i + 3)
+            {
+                for(int j = 1; j <= 9; j++)
+                {
+                    Console.WriteLine($"{i} * {j} = {i*j}");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
